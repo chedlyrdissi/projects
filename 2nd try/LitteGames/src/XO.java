7dabbf1e0e062877edfa17b6970a11cb15b64ff8
chedlyrdissi <chedlyrd2000@gmail.com>
2019-09-12 18:34:09 -0400

    added player controller' git push

--- 2nd try/LitteGames/src/XO.java
+++ 2nd try/LitteGames/src/XO.java
@@ -7,6 +7,7 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class XO implements Game{
@@ -80,6 +81,7 @@ public class XO implements Game{
 		private final Icon OICON=new ImageIcon("Oicon.png");
 		private final Icon EMPTYICON=new ImageIcon("emptyicon.jpg");
 		private JButton[][] field;
+		private JLabel playerLabel=new JLabel();
 		private JPanel buttonpanel=new JPanel(new GridLayout(3,3));
 		private XOModel model;
 		private XOController controller;
@@ -88,7 +90,8 @@ public class XO implements Game{
 			super(new BorderLayout());
 			this.model=model;
 			this.controller=controller;
-			
+			add(playerLabel,BorderLayout.EAST);
+			updatePlayer();
 			add(new JLabel("XO Game"),BorderLayout.NORTH);
 			add(buttonpanel,BorderLayout.CENTER);
 			JButton button;
@@ -103,13 +106,41 @@ public class XO implements Game{
 			}
 		}
 		public void updateButton(int r,int c) {
-			
+			if (model.getCell(r,c)=='X') {
+				this.field[r][c].setIcon(XICON);
+			}else if (model.getCell(r,c)=='O') {
+				this.field[r][c].setIcon(OICON);
+			}
+		}
+		public void updatePlayer() {
+			playerLabel.setText("player "+model.getPlayer()+"'s turn");
 		}
 	}
 	
 	private class XOController implements ActionListener{
+		private XOModel model;
+		private XOView view;
+		public XOController(XOModel model) {
+			this.model=model;
+			this.view=new XOView(model,this);
+		}
 		public void actionPerformed(ActionEvent e) {
-			;
+			String command=e.getActionCommand();
+			int r=Integer.parseInt(command.substring(1,2));
+			int c=Integer.parseInt(command.substring(3,4));
+			if(model.getCell(r,c)==' ') {
+				model.setCell(r,c);
+				view.updateButton(r,c);
+				view.updatePlayer();
+			}
+			switch(model.winner()) {
+			case 'X':JOptionPane.showConfirmDialog(null,"player X wins","GAME OVER",JOptionPane.DEFAULT_OPTION);
+					break;
+			case 'O':JOptionPane.showInternalMessageDialog(null,"player O wins","GAME OVER",JOptionPane.PLAIN_MESSAGE);
+					break;
+			case 'D':JOptionPane.showInternalMessageDialog(null,"It is a draw","GAME OVER",JOptionPane.PLAIN_MESSAGE);
+					break;
+			}
 		}
 	}
 	
