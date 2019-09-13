8fcc646c60d7981be3c210dc348cbcb60cf91b1b
chedlyredissi <chedlyrd2000@gmail.com>
2019-09-13 19:52:13 -0400

    finally works

--- 2nd try/LitteGames/src/XO.java
+++ 2nd try/LitteGames/src/XO.java
@@ -31,6 +31,17 @@ public class XO implements Game{
 				player='X';
 			}
 		}
+		
+		public void reStart() {
+			for(int i=0;i<3;i++) {
+				for(int j=0;j<3;j++) {
+					field[i][j]=' ';
+				}
+			}
+			steps=0;
+			canplay=true;
+			player='X';
+		}
 		public boolean setCell(int r,int c) {
 			if (field[r][c]==' ') {
 				field[r][c]=player;
@@ -41,26 +52,33 @@ public class XO implements Game{
 				return false;
 			}
 		}
+		
 		public boolean done() {
 			return steps==9;
 		}
+		
 		public char winner() {
 			
 			if ((field[0][0]==field[1][1] && field[1][1]==field[2][2] ||
 					field[0][2]==field[1][1] && field[1][1]==field[2][0]) && field[1][1]!=' ' ) {
+				
 				canplay=false;
 				return field[1][1];
+			
 			}
 			
 			for (int i=0;i<3;i++) {
+				
 				if ( field[i][0]==field[i][1] && field[i][1]==field[i][2] && field[i][1]!=' ') {
 					canplay=false;
 					return field[i][0];
 				}
+				
 				if ( field[0][i]==field[1][i] && field[1][i]==field[2][i] && field[1][i]!=' ') {
 					canplay=false;
 					return field[0][i];
 				}
+				
 			}
 			
 			if (!done()) {
@@ -83,30 +101,48 @@ public class XO implements Game{
 		private JButton[][] field=new JButton[3][3];
 		private JLabel playerLabel=new JLabel();
 		private JPanel buttonpanel=new JPanel(new GridLayout(3,3));
+		private JButton resetbutton;
 		private XOModel model;
 		
 		public XOView(XOModel model,XOController controller) {
+			
 			super(new BorderLayout());
 			this.model=model;
-			add(playerLabel,BorderLayout.EAST);
+			
+			JPanel leftpanel=new JPanel();
+			add(leftpanel,BorderLayout.EAST);
+			leftpanel.setLayout(new GridLayout(2,1));
+			
+			leftpanel.add(playerLabel);
+			
+			resetbutton=new JButton("reset");
+			resetbutton.setActionCommand("reset");
+			resetbutton.addActionListener(controller);
+			leftpanel.add(resetbutton);
+			
 			add(new JLabel("XO Game"),BorderLayout.NORTH);
 			add(buttonpanel,BorderLayout.CENTER);
+			
 			for(int i=0;i<3;i++) {
 				for(int j=0;j<3;j++) {
+					
 					field[i][j]=new JButton(EMPTYICON);
 					field[i][j].addActionListener(controller);
 					field[i][j].setActionCommand("("+i+","+j+")");
 					buttonpanel.add(field[i][j]);
+			
 				}
 			}
-			buttonpanel.setPreferredSize(new Dimension(150, 150));
+			
+			buttonpanel.setPreferredSize(new Dimension(300,300));
 		}
 		private void updateButton(int i,int j) {
 			if (model.getCell(i,j)=='O') {
 				this.field[i][j].setIcon(OICON);
-			}
-			if (model.getCell(i,j)=='X') {
+			}else if (model.getCell(i,j)=='X') {
 				this.field[i][j].setIcon(XICON);
+			}else {
+				this.field[i][j].setIcon(EMPTYICON);
 			}
 			
 		}
@@ -129,8 +165,17 @@ public class XO implements Game{
 			view.update();
 		}
 		public void actionPerformed(ActionEvent e) {
+			
+			String command=e.getActionCommand();
+			
+			if (command.equals("reset")) {
+				model.reStart();
+				view.update();
+				return;
+			}
+			
 			if(model.getCanPlay()) {
-				String command=e.getActionCommand();
+				
 				int r=Integer.parseInt(command.substring(1,2));
 				int c=Integer.parseInt(command.substring(3,4));
 				if(model.getCell(r,c)==' ') {
